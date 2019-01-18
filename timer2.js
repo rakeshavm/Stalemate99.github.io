695f65716f9c5122663345832adc9f89c479f26b
mohanpierce99 <paying.rich6990@gmail.com>
2019-01-18 15:43:10 +0530

    Timer2.js

---
+++ timer2.js
@@ -0,0 +1,57 @@
+var TIMER= (function () {
+	var daydisp, hrdisp, mindisp, secdisp;
+	var current, destination,t;
+	var timer;
+
+	function sel(d) {
+		return document.querySelector(d);
+	}
+
+	function doMath() {
+		return {
+			seconds: Math.floor((t / 1000) % 60),
+			minutes: Math.floor((t / 1000 / 60) % 60),
+			hours: Math.floor((t / (1000 * 60 * 60)) % 24),
+			days: Math.floor(t / (1000 * 60 * 60 * 24))
+		}
+	}
+
+	function render(genesis){
+		 daydisp.innerHTML=genesis.days+ "<br> Days";
+		 hrdisp.innerHTML = genesis.hours + "<br> Hours";
+		 mindisp.innerHTML = genesis.minutes + "<br> Minutes";
+		 secdisp.innerHTML = genesis.seconds + "<br> Seconds";
+	}
+
+	function updateClock(){
+		if(!(t>0)){
+			 clearInterval(timer);
+			 return ;
+		}
+	 t-=1000;
+	 render(doMath());
+	}
+
+	function getCurrentTime() {
+		return new Date();
+	}
+
+
+	function init(date, day, hour, min, sec) {
+		daydisp = sel(day);
+		hrdisp = sel(hour);
+		mindisp = sel(min);
+		secdisp = sel(sec);
+		destination = date;
+		current = getCurrentTime();
+		t = destination - current;
+		timer = setInterval(updateClock, 1000);
+
+	}
+
+
+	return {
+		initialize: init
+	}
+})();
+TIMER.initialize(new Date(2019,2,7),"#dayspan","#hrspan","#minspan","#secspan");
